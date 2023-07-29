a1c4f52f419a90ccb87a9e6034c357bb1d63c071
cheshta1828 <cheshtaabatra1828@gmail.com>
2023-07-29 18:37:27 +0530

    some code commented

--- elevatorapp/urls.py
+++ elevatorapp/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('elevator/', ElevatorView.as_view({'get': 'list', 'post': 'create'}), name='elevator-list'),
     path('elevator/<int:pk>/', ElevatorView.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}), name='elevator-detail'),
      path('request_outside_elevator/', ElevatorOutsideRequestView.as_view(), name='elevator-outside-request'),
-    path('elevator_status/', ElevatorStatus.as_view(), name='elevator-status'),
-    path('request_inside_elevator/',ElevatorInsideRequestView.as_view(), name='elevator-inside-request')
+    #path('elevator_status/', ElevatorStatus.as_view(), name='elevator-status'),
+    #path('request_inside_elevator/',ElevatorInsideRequestView.as_view(), name='elevator-inside-request')
 ]
